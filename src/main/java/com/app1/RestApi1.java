baa6288c50300b1216ff1335e18b6a0c5082789c
Vijay Limbasiya <vijay.limbasiya@netcracker.com>
2021-01-12 14:22:12 +0530

    Corrected import statements

--- src/main/java/com/app1/RestApi1.java
+++ src/main/java/com/app1/RestApi1.java
@@ -3,15 +3,11 @@ package com.app1;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api")
--- src/main/java/com/app1/UserService.java
+++ src/main/java/com/app1/UserService.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 @Service
 public class UserService
 {
